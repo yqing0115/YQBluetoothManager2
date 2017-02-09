d502b34a57b2914ff21f4c8ab363c46de5acae69
杨清 <yangqing1989@icloud.com>
2017-02-09 14:40:33 +0800

    Initial Commit

---
+++ YQBluetoothManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQBluetoothManager
+//
+//  Created by 杨清 on 2017/2/9.
+//  Copyright © 2017年 杨清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQBluetoothManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQBluetoothManager
+//
+//  Created by 杨清 on 2017/2/9.
+//  Copyright © 2017年 杨清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
